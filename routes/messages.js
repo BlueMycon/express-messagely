0695ad5759205e18cd5554388fa086b6b5e13bd6
Xiandi Pan <cindypan1807@gmail.com>
2023-05-15 19:22:32 -0400

    message.js

--- routes/messages.js
+++ routes/messages.js
@@ -2,7 +2,8 @@
 
 const Router = require("express").Router;
 const router = new Router();
-
+const Message = require("../models/message");
+const { BadRequestError } = require("../expressError");
 /** GET /:id - get detail of message.
  *
  * => {message: {id,
@@ -15,6 +16,10 @@ const router = new Router();
  * Makes sure that the currently-logged-in users is either the to or from user.
  *
  **/
+router.get("/:id",  ensureLoggedIn, async function (req, res, next) {
+  const message = await Message.get(req.params.id);
+  return res.json({ message });
+});
 
 
 /** POST / - post message.
@@ -23,7 +28,18 @@ const router = new Router();
  *   {message: {id, from_username, to_username, body, sent_at}}
  *
  **/
+router.post("/", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
 
+  const from_username = res.locals.user.username;
+
+  const { to_username, body } = req.body;
+  const message = await Message.create({ from_username, to_username, body });
+  // message = {message: {id, from_username, to_username, body, sent_at}}
+  return res.json({ message });
+
+
+});
 
 /** POST/:id/read - mark message as read:
  *
@@ -32,6 +48,17 @@ const router = new Router();
  * Makes sure that the only the intended recipient can mark as read.
  *
  **/
+router.post("/:id/read", async function (req, res, next) {
+  if (req.body === undefined) throw new BadRequestError();
+
+  const { id } = req.params;
+
+  const message = await Message.markRead({ id });
+
+  return res.json({ message });
+
+
+});
 
 
 module.exports = router;
