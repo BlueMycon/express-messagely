c6d88ede4cc4c719695682c03295f90450098c79
Kenny Burgher <kenny.burgher@gmail.com>
2023-05-15 11:56:44 -0700

    User register, authenticate, updateLoginTimestamp

--- models/user.js
+++ models/user.js
@@ -2,29 +2,74 @@
 
 /** User of the site. */
 
-class User {
+const { NotFoundError, UnauthorizedError } = require("../expressError");
+const db = require("../db");
+const SECRET_KEY = require("../config");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
+class User {
   /** Register new user. Returns
    *    {username, password, first_name, last_name, phone}
    */
 
   static async register({ username, password, first_name, last_name, phone }) {
+    const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
+
+    const result = await db.query(
+      `INSERT INTO users (username,
+                          password,
+                          first_name,
+                          last_name, phone, join_at)
+        VALUES
+          ($1, $2, $3, $4, $5, current_timestamp)
+        RETURNING username, password, first_name, last_name, phone`,
+      [username, hashedPassword, first_name, last_name, phone]
+    );
+
+    return result.rows[0];
   }
 
   /** Authenticate: is username/password valid? Returns boolean. */
 
   static async authenticate(username, password) {
+    const result = await db.query(
+      "SELECT password FROM users WHERE username = $1",
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (user) {
+      if ((await bcrypt.compare(password, user.password)) === true) {
+        const token = jwt.sign({ username }, SECRET_KEY);
+        return res.json({ token });
+      }
+    }
+    throw new UnauthorizedError("Invalid user/password");
   }
 
   /** Update last_login_at for user */
 
   static async updateLoginTimestamp(username) {
+    const result = await db.query(
+      `UPDATE users
+       SET last_login_at = current_timestamp
+         WHERE username = $1
+         RETURNING username, last_login_at`,
+      [username]
+    );
+    const user = result.rows[0];
+
+    if (!user) throw new NotFoundError(`No such user: ${username}`);
+
+    return user;
   }
 
   /** All: basic info on all users:
    * [{username, first_name, last_name}, ...] */
 
   static async all() {
+    
   }
 
   /** Get: get user by username
@@ -36,8 +81,7 @@ class User {
    *          join_at,
    *          last_login_at } */
 
-  static async get(username) {
-  }
+  static async get(username) {}
 
   /** Return messages from this user.
    *
@@ -47,8 +91,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) {
-  }
+  static async messagesFrom(username) {}
 
   /** Return messages to this user.
    *
@@ -58,9 +101,7 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) {
-  }
+  static async messagesTo(username) {}
 }
-
 
 module.exports = User;
