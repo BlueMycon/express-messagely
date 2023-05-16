bdb8852bd0e291a4c4662e68f42e5b1d2a09eb9e
Kenny Burgher <kenny.burgher@gmail.com>
2023-05-16 13:53:23 -0700

    privacy fix

--- config.js
+++ config.js
@@ -12,6 +12,8 @@ const DB_URI = (process.env.NODE_ENV === "test")
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 const TWILIO_AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
+const FROM_PHONE_NUMBER = process.env.FROM_PHONE_NUMBER;
+const TO_PHONE_NUMBER = process.env.TO_PHONE_NUMBER;
 const BCRYPT_WORK_FACTOR = 12;
 
 
@@ -19,5 +21,7 @@ module.exports = {
   DB_URI,
   SECRET_KEY,
   BCRYPT_WORK_FACTOR,
-  TWILIO_AUTH_TOKEN
+  TWILIO_AUTH_TOKEN,
+  FROM_PHONE_NUMBER,
+  TO_PHONE_NUMBER
 };
